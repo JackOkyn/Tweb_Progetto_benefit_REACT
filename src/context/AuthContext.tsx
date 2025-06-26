c507d76be74bb5411af7c276c979c57fe36807ae
JackOkyn <cavallari.jacopo@outlook.it>
2025-06-26 17:30:11 +0200

    login funzionante!

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -27,7 +27,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     /** Ripristina sessione al montaggio */
     useEffect(() => {
         (async () => {
-
             try {
                 const res = await fetch('/session/me', {
                     method: 'GET',
